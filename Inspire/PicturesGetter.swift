f33215fe2ca211c241224fff86d8037a269879d4
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-09-24 01:04:51 -0500

    Finished a method to escape dangerous characters in a query

--- Inspire/PicturesGetter.swift
+++ Inspire/PicturesGetter.swift
@@ -17,5 +17,22 @@ class PictureGetter {
         
         
     }
-    
+
+    /**
+        Take in a dictionary of keys and values array and convert it into query safe string
+    */
+    private func getUsableParamsForQuery(parameters: [String:AnyObject]) -> String {
+        if parameters.isEmpty {
+            return ""
+        }
+        
+        var pairs = [String]()
+        for (key, value) in parameters {
+            // replacing all characters not in the specified set with percent encoded characters.
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
+            pairs.append(key + "=" + "\(escapedValue!)")
+        }
+        
+        return "?\(pairs.joined(separator: "&"))"
+    }
 }
