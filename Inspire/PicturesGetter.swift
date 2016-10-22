4874c886ce7538e53150770083fb2cbec9bdfaea
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-10-22 14:55:50 -0500

    Added photos into array

--- Inspire/PicturesGetter.swift
+++ Inspire/PicturesGetter.swift
@@ -42,112 +42,20 @@ class PictureGetter {
             Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
         ]
         
-        Alamofire.request(Constants.Flickr.APIBaseURL, parameters: methodParametersForSearch).responseJSON { response in
-            if let value = response.result.value {
+        Alamofire.request(Constants.Flickr.APIBaseURL, parameters: methodParametersForSearch)
+            .responseJSON { response in
+                guard let value = response.result.value else {
+                    print("Error in getting URLS")
+                    print(response.result.error)
+                    return
+                }
+                
+                
                 let json = JSON(value)
-                print(json["photos"].dictionaryValue)
-//                print(value)
+                for photo in (json["photos"].dictionaryValue["photo"]?.array)! {
+                    let temp = Photo(url: photo["url_m"].stringValue, title: photo["title"].stringValue)
+                    self.photos.append(temp)
+                }
             }
-        }
-        
-        
-        
-//
-//        // create url and request
-//        let session = URLSession.shared
-//        let urlString = Constants.Flickr.APIBaseURL + getUsableParamsForQuery(parameters: methodParametersForSearch)
-//        let url = URL(string: urlString)!
-//        let request = URLRequest(url: url)
-//        
-//        // create network request
-//        let task = session.dataTask(with: request) { (data, response, error) in
-//            
-//            // check for error
-//            guard (error == nil) else {
-//                print("PicturesGetter: Error with data task request!")
-//                return
-//            }
-//            
-//            // check for good response
-//            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200, statusCode <= 299 else {
-//                print("PicturesGetter: Status Code is not 2xx!")
-//                return
-//            }
-//            
-//            guard let data = data else {
-//                print("PicturesGetter: No data!")
-//                return
-//            }
-//            
-//            // parsing data
-//            let parsedResult: NSDictionary!
-//            do {
-//                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
-//            } catch {
-//                print("PicturesGetter: Could not parse the data as JSON: '\(data)'")
-//                return
-//            }
-//            
-//            // check error from Flickr API
-//            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
-//                print("PicturesGetter: Flickr API returned error code: \(parsedResult)")
-//                return
-//            }
-//            
-//            // get array of photos
-//            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject],
-//                let photos = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] else {
-//                    print("PicturesGetter: Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' and '\(Constants.FlickrResponseKeys.Photo)' in \(parsedResult)")
-//                    return
-//            }
-//            
-//            var tempPhotos: [Photo] = []
-//            
-//            // get all the info of the photo
-//            for photo in photos {
-//                
-//                // TODO: Create a Photo class and store the data into it instead
-//                guard let url = photo[Constants.FlickrResponseKeys.MediumURL] as? String else {
-//                    print("PicturesGetter: Cannot find keys '\(Constants.FlickrResponseKeys.MediumURL) in \(photo)")
-//                    return
-//                }
-//                
-//                guard let title = photo[Constants.FlickrResponseKeys.Title] as? String else {
-//                    print("PicturesGetter: Cannot find keys '\(Constants.FlickrResponseKeys.Title) in \(photo)")
-//                    return
-//                }
-//                
-//                let tempPhoto = Photo(url: url, title: title)
-//                tempPhoto.requestImage()
-//                
-//                tempPhotos.append(tempPhoto)
-//            }
-//            
-//            DispatchQueue.main.async {
-//                self.photos = tempPhotos
-//                functionToExecuteAtTheEnd()
-//            }
-//        }
-//        
-//        // start the task
-//        task.resume()
-    }
-
-    /**
-        Take in a dictionary of keys and values array and convert it into query safe string
-    */
-    private func getUsableParamsForQuery(parameters: [String: String]) -> String {
-        if parameters.isEmpty {
-            return ""
-        }
-        
-        var pairs = [String]()
-        for (key, value) in parameters {
-            // replacing all characters not in the specified set with percent encoded characters.
-            let escapedValue = value.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
-            pairs.append(key + "=" + "\(escapedValue!)")
-        }
-        
-        return "?\(pairs.joined(separator: "&"))"
     }
 }
