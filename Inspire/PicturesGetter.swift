9c7353d83f645f71b32ab381e3be9f1b8128aa58
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-10-22 20:53:15 -0500

    Cut out the promise part because it may decrease performance

--- Inspire/PicturesGetter.swift
+++ Inspire/PicturesGetter.swift
@@ -58,75 +58,71 @@ class PictureGetter {
                     let temp = Photo(id: photoJSON[Constants.FlickrResponseKeys.PhotoID].stringValue, url: photoJSON[Constants.FlickrResponseKeys.MediumURL].stringValue, title: photoJSON[Constants.FlickrResponseKeys.Title].stringValue)
                     self.photos[Int(temp.id)!] = temp // add to the list right away, so that the app can display the image first as soon as possible
                     
-                    // Secondly, for each photo, we request its info. The promises are unessary, but it makes the code more readable.
-                    
-                    self.getEXIFInfo(photoID: temp.id)
-                    .then { result -> Void in
-                        
-                        let photo = self.photos[result.0]!
-                        let exifs = result.1[Constants.FlickrResponseKeys.Photo][Constants.FlickrResponseKeys.EXIF].arrayValue
-                        
-//                        print(result.1)
-//                        print()
-                        
-                        for exif in exifs {
-                            
-//                            print(exif)
-                            
-                            let content = exif[Constants.FlickrResponseKeys.Raw][Constants.FlickrResponseKeys.Content].stringValue
-                            
-                            switch (exif[Constants.FlickrResponseKeys.Tag].stringValue) {
-                            case Constants.FlickrResponseKeys.Make:
-                                photo.cameraMake = content
-                                print(content)
-                                break
-                            case Constants.FlickrResponseKeys.Model:
-                                break
-                            case Constants.FlickrResponseKeys.ShuttleSpeed:
-                                break
-                            case Constants.FlickrResponseKeys.Aperture:
-                                break
-                            case Constants.FlickrResponseKeys.ISO:
-                                break
-                            case Constants.FlickrResponseKeys.FocalLength:
-                                break
-                            case Constants.FlickrResponseKeys.Lens:
-                                break
-                            default:
-                                break // do not do anything otherwise
-                            }
-                        }
-                        
-                    }.catch { error in
-                        print("Error in getting photo EXIF")
-                        print(error)
-                    }
-                    
-                    
+                    // Secondly, for each photo, we request its info.
+                    self.getEXIFInfo(photoID: Int(temp.id)!)
                 }
-            }
+        }
     }
     
-    func getEXIFInfo(photoID: String) -> Promise<(Int, JSON)> {
-        return Promise { solve, reject in
-            
+    func getEXIFInfo(photoID: Int) {
+        
             let methodParametersForGetEXIF = [
                 Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.MethodGetEXIF,
                 Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
-                Constants.FlickrParameterKeys.PhotoID: photoID,
+                Constants.FlickrParameterKeys.PhotoID: String(photoID),
                 Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.Format,
                 Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
             ]
             
             Alamofire.request(Constants.Flickr.APIBaseURL, parameters: methodParametersForGetEXIF)
                 .responseJSON { response in
-                    if let value = response.result.value {
-                        let json = JSON(value)
-                        solve((Int(photoID)!, json))
-                    } else {
-                        reject(response.result.error!)
+                    
+                    guard let value = response.result.value else {
+                        print("Cannot convert to EXIF")
+                        print(response.result.error)
+                        return
+                    }
+
+                    let json = JSON(value)
+                    
+                    guard let photo: Photo = self.photos[photoID] else {
+                        print("No such photo id: \(photoID) in the database")
+                        return
+                    }
+                    
+                    let exifs = json[Constants.FlickrResponseKeys.Photo][Constants.FlickrResponseKeys.EXIF].arrayValue
+                    
+                    for exif in exifs {
+                        
+                        let content = exif[Constants.FlickrResponseKeys.Raw][Constants.FlickrResponseKeys.Content].stringValue
+//                        print("TAG: \(exif[Constants.FlickrResponseKeys.Tag].stringValue) \t \(content)")
+                        
+                        switch (exif[Constants.FlickrResponseKeys.Tag].stringValue) {
+                        case Constants.FlickrResponseKeys.
+                            photo.cameraMake = content
+                            break
+                        case Constants.FlickrResponseKeys.Model:
+                            photo.cameraModel = content
+                            break
+                        case Constants.FlickrResponseKeys.ShuttleSpeed:
+                            photo.shuttleSpeed = content
+                            break
+                        case Constants.FlickrResponseKeys.Aperture:
+                            photo.aperature = content
+                            break
+                        case Constants.FlickrResponseKeys.ISO:
+                            photo.iso = content
+                            break
+                        case Constants.FlickrResponseKeys.FocalLength:
+                            photo.focalLength = content
+                            break
+                        case Constants.FlickrResponseKeys.Lens:
+                            photo.lensModel = content
+                            break
+                        default:
+                            break // do not do anything otherwise
+                        }
                     }
             }
-        }
     }
 }
